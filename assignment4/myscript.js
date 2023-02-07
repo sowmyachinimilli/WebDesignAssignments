b63bad62934cc54dccf15fe153ccf8bf6356736a
sowmyachinimilli <chinimillisowmya@gmail.com>
2023-02-07 16:32:52 -0500

    creating table on submit

--- assignment4/myscript.js
+++ assignment4/myscript.js
@@ -4,7 +4,7 @@ var ratingFieldCreated = false;
 var fromInside = false;
 
 //Function to validate Title
-function validateTitle() {
+function checkTitle() {
     var radios = document.getElementsByName("title");
     var selRds = 0;
     for (var i = 0, len = radios.length; i < len; i++) {
@@ -13,6 +13,7 @@ function validateTitle() {
         }
     }
     if (selRds > 0) {
+        document.getElementById("error_title").style.display = "none";
         validationFailed.push("false");
     }
     else {
@@ -32,6 +33,7 @@ function validateInputs(object, regCase, divId) {
     var regExCity = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
     var regExState = /^(([Aa][EeLlKkSsZzRr])|([Cc][AaOoTt])|([Dd][EeCc])|([Ff][MmLl])|([Gg][AaUu])|([Hh][Ii])|([Ii][DdLlNnAa])|([Kk][SsYy])|([Ll][Aa])|([Mm][EeHhDdAaIiNnSsOoTt])|([Nn][EeVvHhJjMmYyCcDd])|([Mm][Pp])|([Oo][HhKkRr])|([Pp][WwAaRr])|([Rr][Ii])|([Ss][CcDd])|([Tt][NnXx])|([Uu][Tt])|([Vv][TtIiAa])|([Ww][AaVvIiYy]))$/;
     var regExZipCode = /^[0-9]{5}(?:-[0-9]{4})?$/;
+    var regExComments = /^[a-zA-Z0-9@=\-'"]+/;
 
     var regToVal;
 
@@ -58,16 +60,9 @@ function validateInputs(object, regCase, divId) {
         case "zipcode":
             regToVal = regExZipCode;
             break;
-        case 8:
-            regToVal = regExHduh;
-            break;
         case "comments":
             regToVal = regExComments;
             break;
-        case "rating":
-            regToVal = regExRating;
-            break;
-
     }
 
     if (!object.value.trim().match(regToVal)) {
@@ -106,6 +101,18 @@ function removeDivs(object, divId){
     document.getElementById(divId).style.display = "none";
 }
 
+function emptyCheck(object,divid){
+    if (object.value == "" || object.value == null) {
+        object.style.border = "2px solid red";
+        document.getElementById(divid).style.display = "block";
+        validationFailed.push("true");
+    } else {
+        object.style.border = "";
+        document.getElementById(divid).style.display = "none";
+        validationFailed.push("false");
+    }
+}
+
 function clearDivs(){
     var inplist = document.getElementsByClassName("validation");
     var inpDivlist = document.getElementsByClassName("validationDiv");
@@ -120,3 +127,120 @@ function clearDivs(){
     document.getElementById("error_hduh").style.display = "none";
 }
 
+function formValidation(){
+    var inplist = document.getElementsByClassName("validation");
+    var inpDivlist = document.getElementsByClassName("validationDiv");
+
+    for (var i = 0; i < inplist.length; i++) {
+        emptyCheck(inplist[i],inpDivlist[i].id);
+    }
+    console.log("Before validateTitle validationFailed.length + " + validationFailed.length);
+    console.log("Before validateTitle validationFailed.includes + " + validationFailed.includes("true"))
+    checkTitle();
+    validateHduh();
+    console.log("After validateHduh validationFailed.length + " + validationFailed.length);
+    console.log("After validateHduh validationFailed.includes + " + validationFailed.includes("true"));
+    if(validationFailed.includes("true")){
+        return true;
+    };
+    return false;
+}
+
+function getTitle(rdg) {
+    var els = document.getElementsByName(rdg);
+    for (var i = 0, l = els.length; i < l; i++) {
+        if (els[i].checked) {
+            return els[i].value;
+        }
+    }
+}
+
+function getCheckboxValues(cbv) {
+    console.log("Inside getCheckboxValues : " + cbv);
+    var checkedValue = [];
+    var cbeles = document.getElementsByName(cbv);
+    for (var i = 0; cbeles[i]; ++i) {
+        if (cbeles[i].checked) {
+            checkedValue.push(cbeles[i].value);
+        }
+
+    }
+    console.log("checkedValue : " + checkedValue);
+    return checkedValue;
+}
+
+function submitForm() {
+    console.log("InsidesubmitForm ");
+    validationFailed = [];
+    if(formValidation()){
+        return;
+    }
+    console.log("after formValidation ");
+
+    //var dishType = document.getElementById("dishType");
+    data = {
+        "title": getTitle('title'),
+        "fname": document.getElementById('firstName').value,
+        "lname": document.getElementById('lastName').value,
+        "email": document.getElementById('emailId').value,
+        "phone": document.getElementById('phoneNumber').value,
+        "streetAddress1": document.getElementById('streetAddress1').value,
+        "streetAddress2": document.getElementById('streetAddress2').value,
+        "city": document.getElementById('city').value,
+        "state": document.getElementById('state').value,
+        "zipcode": document.getElementById('zipcode').value,
+        //"dishType": dishType.options[dishType.selectedIndex].text,
+        //"type": getCheckboxValues("service"),
+        //"rating": document.getElementById('rating').value,
+        "hduh": getCheckboxValues("source"),
+        "comments": document.getElementById('comments').value,
+    }
+    console.log("data: " + data);
+
+    var tab = document.getElementById("myTable");
+    tab.style.display = "block";
+
+    var row = tab.insertRow();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
+    var cell4 = row.insertCell();
+    var cell5 = row.insertCell();
+    var cell6 = row.insertCell();
+    var cell7 = row.insertCell();
+    var cell8 = row.insertCell();
+    var cell9 = row.insertCell();
+    var cell10 = row.insertCell();
+    var cell11 = row.insertCell();
+    var cell12 = row.insertCell();
+    // var cell13 = row.insertCell();
+    // var cell14 = row.insertCell();
+    // var cell15 = row.insertCell();
+
+    console.log("before assigning data");
+
+    cell1.innerHTML = data.title;
+    cell2.innerHTML = data.fname;
+    cell3.innerHTML = data.lname;
+    cell4.innerHTML = data.email;
+    cell5.innerHTML = data.phone;
+    cell6.innerHTML = data.streetAddress1;
+    cell7.innerHTML = data.streetAddress2;
+    cell8.innerHTML = data.city;
+    cell9.innerHTML = data.state;
+    cell10.innerHTML = data.zipcode;
+    // cell11.innerHTML = data.dishType;
+    // cell12.innerHTML = data.type;
+    // cell13.innerHTML = data.rating;
+    cell11.innerHTML = data.hduh;
+    cell12.innerHTML = data.comments;
+
+    console.log("before appending data");
+
+    tab.appendChild(row);
+
+    $('#myForm')[0].reset();
+
+    return false;
+
+}
